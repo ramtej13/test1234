556a983d29e05afbdd3efea560cad8ffb1c3f8b8
ramtej13 <ramtej.j13@gmail.com>
2020-09-17 04:32:00 +0530

    test

--- ramtejajakka/asgi.py
+++ ramtejajakka/asgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 import django
 from channels.routing import get_default_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ramtejajakka.settings')
 django.setup()
 application = get_default_application()
